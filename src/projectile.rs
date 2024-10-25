e3e98829ee5d5d6ec1541593349ce49bf7ad157b
ProfessionalMenace <vondracekadam00@gmail.com>
2024-10-25 16:32:18 +0200

    Update to projectile spawn system + ran rustfmt

--- src/projectile.rs
+++ src/projectile.rs
@@ -12,46 +12,52 @@ impl Plugin for ProjectilePlugin {
 
 #[allow(dead_code)]
 #[derive(Resource)]
-pub struct ProjectileSpawner
-{
+pub struct ProjectileSpawner {
     pub default: Projectile,
-    pub spawner: HashMap<String, Projectile>,
+    pub spawner: HashMap<String, ProjectileBundle>,
 }
 
-fn projectiles_spawner(
-    mut commands: Commands,
-    _asset_server: Res<AssetServer>,
-) {
+fn projectiles_spawner(mut commands: Commands, asset_server: Res<AssetServer>) {
     let projectiles_spawner = ProjectileSpawner {
-        default: Projectile{ damage: 0, velocity: Vec3::splat(0.0), radius: 0.0 }, 
-        spawner: HashMap::from([
-        (String::from("default"), Projectile{ damage: 32, velocity: Vec3::new(32.0, 0.0, 0.0), radius: 16.0 }),
-        ]),
+        default: Projectile {
+            damage: 0,
+            velocity: Vec3::splat(0.0),
+            radius: 0.0,
+        },
+        spawner: HashMap::from([(
+            String::from("default"),
+            ProjectileBundle {
+                projectile: Projectile {
+                    damage: 32,
+                    velocity: Vec3::new(32.0, 0.0, 0.0),
+                    radius: 16.0,
+                },
+                sprite: SpriteBundle {
+                    texture: asset_server.load("Projectiles/Default.png"),
+                    ..default()
+                },
+            },
+        )]), 
     };
     commands.insert_resource(projectiles_spawner);
 }
 
 #[allow(dead_code)]
 #[derive(Component, Clone, Copy)]
-pub struct Projectile
-{
+pub struct Projectile {
     pub damage: i32, // unused
     pub velocity: Vec3,
     pub radius: f32, // unused
 }
 
-#[derive(Bundle)]
-pub struct ProjectileBundle
-{
+#[derive(Bundle, Clone)]
+pub struct ProjectileBundle {
     pub projectile: Projectile,
-    pub sprite_bundle: SpriteBundle,
+    pub sprite: SpriteBundle,
 }
 
 // update every frame, linear movement
-pub fn projectiles_move(
-    time: Res<Time>,
-    mut query: Query<(&Projectile, &mut Transform)>,
-){
+pub fn projectiles_move(time: Res<Time>, mut query: Query<(&Projectile, &mut Transform)>) {
     for (projectile, mut transform) in &mut query {
         let displacement = projectile.velocity * time.delta_seconds();
         transform.translation += displacement;
@@ -60,15 +66,14 @@ pub fn projectiles_move(
 
 // maybe combine into singular function
 pub fn projectiles_despawn(
-    mut commands: Commands, 
+    mut commands: Commands,
     windows: Query<&Window>,
     entities: Query<(Entity, &Transform), With<Projectile>>,
-){
+) {
     let window = windows.single();
-    for (entity, transform) in entities.iter(){
+    for (entity, transform) in entities.iter() {
         // add better despawns check
-        if transform.translation.x > window.width()/2.0
-        {
+        if transform.translation.x > window.width() / 2.0 {
             commands.entity(entity).remove::<ProjectileBundle>();
         }
     }
--- src/tower.rs
+++ src/tower.rs
@@ -1,37 +1,38 @@
-use std::time::Duration;
+use crate::health::Health;
+use crate::projectile::*;
 use bevy::prelude::*;
 use bevy::time::Stopwatch;
-use crate::projectile::*;
-use crate::health::Health;
+use std::collections::HashMap;
+use std::time::Duration;
 
 pub struct TowerPlugin;
-impl Plugin for TowerPlugin
-{
+impl Plugin for TowerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, tower_update);
     }
 }
 
-#[derive(Component)]
-pub struct Tower
-{
+#[derive(Component, Clone)]
+pub struct Tower {
     pub stopwatch: Stopwatch,
     pub cooldown: Duration,
 }
 
-#[derive(Bundle)]
-pub struct TowerBundle
-{
+#[derive(Bundle, Clone)]
+pub struct TowerBundle {
     pub tower: Tower,
     pub health: Health,
     pub sprite: SpriteBundle,
 }
 
+#[allow(dead_code)]
+#[derive(Resource)]
+pub struct TowerSpawner {
+    pub spawner: HashMap<String, TowerBundle>, // tower name -> Tower + Health + Sprite + Projectile
+}
+
 // creating tower from tower_id (not implemented)
-pub fn tower_create(
-    pos: Vec2,
-    image_handle: Handle<Image>,
-) -> TowerBundle {
+pub fn tower_create(pos: Vec2, image_handle: Handle<Image>) -> TowerBundle {
     let translation = Vec3::new(pos.x, pos.y, 0.0);
     TowerBundle {
         tower: Tower {
@@ -43,34 +44,24 @@ pub fn tower_create(
             texture: image_handle,
             transform: Transform::from_translation(translation),
             ..default()
-        }
+        },
     }
 }
 
-// tower spawn projectile in time intervals
+// tower spawn projectile in time interval
 pub fn tower_update(
     time: Res<Time>,
     mut commands: Commands,
-    asset_server: Res<AssetServer>, // maybe posible to remove
-    spawner : Res<ProjectileSpawner>, // should contain sprite handle
+    spawner: Res<ProjectileSpawner>,
     mut query: Query<(&mut Tower, &Transform)>,
-){
-    for (mut tower, transform) in &mut query
-    {
-        if tower.stopwatch.elapsed() > tower.cooldown
-        {
-            let Some(projectile) = spawner.spawner.get("default") else {
-                continue; // skips the reset -> tries spawn every time
+) {
+    for (mut tower, transform) in &mut query {
+        if tower.stopwatch.elapsed() > tower.cooldown {
+            if let Some(projectile_bundle) = spawner.spawner.get("default") {
+                let mut new_pb = (*projectile_bundle).clone();
+                new_pb.sprite.transform.translation = transform.translation.clone();
+                commands.spawn(new_pb); // too much clone?
             };
-            let bundle = ProjectileBundle{
-                projectile: projectile.clone(),
-                sprite_bundle: SpriteBundle{
-                    transform: Transform::from_translation(transform.translation),
-                    texture: asset_server.load("Projectiles/Default.png"),
-                    ..default()
-                },
-            };
-            commands.spawn(bundle);
             tower.stopwatch.reset();
         }
         tower.stopwatch.tick(time.delta());
